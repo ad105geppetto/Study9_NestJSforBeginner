a7c2c7dd9e233f2f94c2363d6a17edcae3a53f34
ad105geppetto <ad105geppetto@gmail.com>
2022-07-30 21:20:42 +0900

    [feat] Add E2E Testing PATCH and DELETE movies id

--- hi-nest/test/app.e2e-spec.ts
+++ hi-nest/test/app.e2e-spec.ts
@@ -45,6 +45,17 @@ describe('AppController (e2e)', () => {
         })
         .expect(201)
     })
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: "TEST",
+          year: 2000,
+          genres: ['test'],
+          other: "thing"
+        })
+        .expect(400)
+    })
 
     it('DELETE', () => {
       return request(app.getHttpServer())
@@ -54,13 +65,17 @@ describe('AppController (e2e)', () => {
   })
 
   describe('/movies/:id', () => {
-    it.todo('GET 200', () => {
+    it('GET 200', () => {
       return request(app.getHttpServer()).get("/movies/1").expect(200)
     })
-    it.todo('GET 404', () => {
+    it('GET 404', () => {
       return request(app.getHttpServer()).get("/movies/999").expect(404)
     })
-    it.todo('DELETE')
-    it.todo('PATCH')
+    it('PATCH 200', () => {
+      return request(app.getHttpServer()).patch('/movies/1').send({ title: 'Updated Test' }).expect(200)
+    })
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200)
+    })
   })
 });
